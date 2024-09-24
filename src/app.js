fbccc702efb5ef9cc9db5458127d59e9278c6295
Venkataramana hegde <venkataramanahegde64@gmail.com>
2024-09-24 18:48:22 +0530

    add utils code

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
-import express from "express"   
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 
 const app = express()
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 
 
+//Configurations using middlewares
+app.use(express.json({limit: "16kb"}))          //middleware configuraion to allow json through express
+app.use(express.urlencoded({extended: true, limit: "16kb"}))   
+app.use(express.static("public"))               //store any pdfs or images in server can be accessed public
+app.use(cookieParser())                         //middleware to access and send cookies to user browser 
 export { app }
