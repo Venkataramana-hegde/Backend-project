b68266551b8fca7bbb12dd92dde11a416433f3a2
Venkataramana hegde <venkataramanahegde64@gmail.com>
2024-12-11 22:32:32 +0530

    add subscribers aggregation pipelines

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -4,14 +4,11 @@ const subscriptionSchema = new Schema({
     subscriber:{
         type: Schema.Types.ObjectId, //one who is subscribing
         ref: "User",
-    }
+    },
     channel: {
         type: Schema.Types.ObjectId, //one to whom subscriber is subscribing
         ref: "User",
-    },
-    {
-      timestamp: true  
     }
-})
+},{timestamp: true})
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema)
